533246f8e8953c1fcc5f919ea5661b2cfec41031
Diego Ihara <dihara@gmail.com>
2020-03-14 14:30:30 -0500

    return cluster

--- randcluster.py
+++ randcluster.py
@@ -14,10 +14,6 @@ def scoop_points(word, all_words, word_embedding, radius, alpha):
             
 
 def random_cluster(word_embedding):
-    # Delta = ?
-    # random permutation of points
-    # sample Gamma [1/2, 1/4]
-    # scoop balls of radius Delta*Gamma
     words = list(word_embedding)
     random.shuffle(words)
     
@@ -30,22 +26,25 @@ def random_cluster(word_embedding):
                 max_dist = np.linalg.norm(word_embedding[words[i]] - word_embedding[words[j]])
     
     clusters = []
-    #mean_distance = sum_dist / c
+    
     while len(X) > 0:
         alpha = np.random.uniform(0.25, 0.5)
         s, X = scoop_points(X[0], X, word_embedding, max_dist/16, alpha)    
-        print("scoop of size", len(s))
         
         clusters.append(s)
 
-    print(clusters)
+    return clusters
 
 if __name__ == "__main__":
-    X = [np.array([i for x in range(100)]) for i in range(100)]
-    w = [str(i) for i in range(100)]
+    s = 1000
+    d = 100
+    
+    X = [np.array([i for x in range(d)]) for i in range(s)]
+    w = [str(i) for i in range(s)]
     
     vocab = {}
     for i in range(len(X)):
         vocab[w[i]] = X[i]
     
-    random_cluster(vocab)
+    c = random_cluster(vocab)
+    print(c)
